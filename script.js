d837485496db36cf677f41be3458c9210bb83912
Mikaela Hedberg <mie@du.se>
2022-11-15 20:28:58 +0100

    js-rendered svg + midi

--- script.js
+++ script.js
@@ -1,95 +1,98 @@
-/*  */
-const clav = document.getElementById('clav');
+const sizeFactor = 2;
+const octCount = 2;
+const octStart = 4;
 
-const base = {
-  xStart: 0,
-  yStart: 0,
-  white: {
-    width: 50,
-    height: 220,
-    style:
-      'fill:#ffffff;fill-opacity:1;stroke:#000000;stroke-width:2.77070045;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.98009949',
-    keys: ['c1', 'd1', 'e1', 'f1', 'g1', 'a1', 'b1', 'c2', 'd2', 'e2', 'f2', 'g2', 'a2', 'b2', 'c3', 'd3', 'e3']
-  },
-  black: {
-    width: 31,
-    height: 133,
-    gap1: 20,
-    gap2: 30,
-    style:
-      '#000000;fill-opacity:1;stroke:#000000;stroke-width:2.82222223;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1',
-    keys: ['db1', 'eb1', 'gb1', 'ab1', 'bb1', 'db2', 'eb2', 'gb2', 'ab2', 'bb2', 'db3', 'eb']
-  }
+const white = {
+  width: 23,
+  height: 120,
+  count: 7,
+  id: ['c', 'd', 'e', 'f', 'g', 'a', 'b'],
+  code: [0, 2, 4, 5, 7, 9, 11]
+};
+const black = {
+  width: 13,
+  height: 80,
+  count: 5,
+  baseX: [14.33333, 41.66666, 82.25, 108.25, 134.75],
+  id: ['dFlat', 'eFlat', 'gFlat', 'aFlat', 'bFlat'],
+  code: [1, 3, 6, 8, 10]
 };
 
-const keyId = [
-  'c1',
-  'db1',
-  'd1',
-  'eb1',
-  'e1',
-  'f1',
-  'gb1',
-  'g1',
-  'ab1',
-  'a1',
-  'bb1',
-  'b1',
-  'c2',
-  'db2',
-  'd2',
-  'eb2',
-  'e2',
-  'f2',
-  'gb2',
-  'g2',
-  'ab2',
-  'a2',
-  'bb2',
-  'b2',
-  'c3',
-  'db3',
-  'd3',
-  'eb3',
-  'e3'
-];
+const xmlns = 'http://www.w3.org/2000/svg';
 
-keyId.forEach((key) => {
-  const type = base.white.keys.includes(key) ? 'white' : 'black';
-  rect = `
-          <rect
-            id="${key}"
-            data-type=${type}
-            width="${base[type].width}"
-            height="${base[type].height}"          
-            style="${base[type].style}"
-        />
-    `;
+const svg = document.implementation.createDocument(xmlns, 'svg', null);
+const piano = svg.documentElement;
 
-  clav.insertAdjacentHTML('beforeend', rect);
-});
+const colorBlack = 'rgb(58, 56, 69)';
+const colorBlue = 'rgb(86, 55, 133)';
 
-const existingKeys = [...clav.children];
+const octWidth = white.width * white.count * sizeFactor;
+piano.setAttribute('width', `${octWidth * octCount}px`);
+piano.setAttribute('height', `${white.height * sizeFactor}px`);
+piano.setAttribute('xml:space', 'preserve');
 
-existingKeys.forEach((key, i) => {
-  const prevKey = key.previousElementSibling;
+for (let i = 0; i < octCount; i++) {
+  const oct = document.createElementNS(xmlns, 'g');
+  oct.style.transform = `translateX(${octWidth * i}px)`;
+  const realOct = i + octStart;
+  oct.setAttributeNS(null, 'id', `oct${realOct}`);
 
-  console.log(prevKey?.getAttribute('data-type'));
-});
+  for (let wI = 0; wI < white.count; wI++) {
+    const whiteKey = document.createElementNS(xmlns, 'rect');
+    whiteKey.style.fill = 'white';
+    whiteKey.style.stroke = colorBlack;
+    whiteKey.setAttributeNS(null, 'data-key', `${white.id[wI]}-${realOct}`);
+    whiteKey.setAttributeNS(null, 'data-color', 'white');
+    whiteKey.setAttributeNS(null, 'x', wI * white.width * sizeFactor);
+    whiteKey.setAttributeNS(null, 'y', '0');
+    whiteKey.setAttributeNS(null, 'width', white.width * sizeFactor);
+    whiteKey.setAttributeNS(null, 'height', white.height * sizeFactor);
+    whiteKey.setAttributeNS(null, 'id', white.code[wI] + 12 * realOct);
 
-/* keys.black.instances.forEach((key, i) => {
-  const rect = `
-        <rect
-          id="${key}"
-          width="${keys.black.width}"
-          height="${keys.black.height}"          
-          style="${keys.black.style}"
-      />
-  `;
+    oct.appendChild(whiteKey);
+  }
+  for (let bI = 0; bI < black.count; bI++) {
+    const blackKey = document.createElementNS(xmlns, 'rect');
+    blackKey.style.fill = colorBlack;
+    blackKey.style.stroke = 'white';
+    blackKey.setAttributeNS(null, 'data-key', `${black.code[bI]}-${realOct}`);
+    blackKey.setAttributeNS(null, 'x', black.baseX[bI] * sizeFactor);
+    blackKey.setAttributeNS(null, 'y', '0');
+    blackKey.setAttributeNS(null, 'width', black.width * sizeFactor);
+    blackKey.setAttributeNS(null, 'height', black.height * sizeFactor);
+    blackKey.setAttributeNS(null, 'id', black.code[bI] + 12 * realOct);
 
-  clav.insertAdjacentHTML('beforeend', rect);
-  const newRect = document.getElementById(key);
-  console.log(newRect);
-  console.log(newRect.previousElementSibling);
-  /* newRect.setAttribute('x', newRect.previousElementSibling.x + keys.white.width / 2); 
-});*/
+    oct.appendChild(blackKey);
+  }
+
+  piano.appendChild(oct);
+}
+document.getElementById('svgRoot').appendChild(piano, true);
+
+/* Midi */
+
+if (navigator.requestMIDIAccess) {
+  console.log('This browser supports WebMIDI!');
+} else {
+  console.log('WebMIDI is not supported in this browser.');
+}
+
+navigator.requestMIDIAccess().then(onMIDISuccess, () => console.log('Could not access keyboard'));
+
+function onMIDISuccess(midiAccess) {
+  console.log(midiAccess);
+
+  for (var input of midiAccess.inputs.values()) {
+    input.onmidimessage = getMIDIMessage;
+  }
+}
+
+function getMIDIMessage(midiMessage) {
+  const currKey = document.getElementById(midiMessage.data[1]);
+  if (midiMessage.data[0] === 144) {
+    console.log(currKey.getAttributeNS(null, 'data-key'));
+    currKey.style.fill = colorBlue;
+  } else {
+    currKey.style.fill = 'white';
+  }
+}
